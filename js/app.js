9e847dc00b138da439c8501392c7bb6d16d912cd
FrancescoDOrtona <francescod.97@hotmail.it>
2023-10-10 18:21:54 +0200

    Add: JS prev & next img

--- js/app.js
+++ js/app.js
@@ -4,29 +4,69 @@
 // - creo un array contente le immagini
 // - recupero il Dom element tramite la classe
 
-const carouselImages = ['./img/01.webp', './img/02.webp','./img/03.webp','./img/04.webp','./img/05.webp'];
+const carouselImages = [
+  "./img/01.webp",
+  "./img/02.webp",
+  "./img/03.webp",
+  "./img/04.webp",
+  "./img/05.webp",
+];
 console.log(carouselImages);
 
-const carouselDOMElement = document.querySelector('.carousel');
+const carouselDOMElement = document.querySelector(".carousel");
 console.log(carouselDOMElement);
 
 // - Creo un ciclo for con il quale vado ad inserire le immagini nel Dom
-for(let i = 0; i < carouselImages.length; i++){
-    const currentImage = carouselImages[i];
-    console.log(currentImage);
+for (let i = 0; i < carouselImages.length; i++) {
+  const currentImage = carouselImages[i];
+  console.log(currentImage);
 
-    const imgString = `
+  const imgString = `
     <div class="carousel__img">
         <img class="img" src="${currentImage}"
     </div>
-    `
-    console.log(imgString)
+    `;
+  console.log(imgString);
 
-    carouselDOMElement.innerHTML += imgString
+  carouselDOMElement.innerHTML += imgString;
 }
 
+let actualImgIdx = 0;
+
 // Mostrare un'immagine alla volta
- const carouselImgDOMElements = document.getElementsByClassName('carousel__img')
- const firstImageDOMElement = carouselImgDOMElements[0]
- firstImageDOMElement.classList.add('active')
-console.log(firstImageDOMElement)
+const carouselImgDOMElements = document.getElementsByClassName("carousel__img");
+const firstImageDOMElement = carouselImgDOMElements[actualImgIdx];
+firstImageDOMElement.classList.add("active");
+console.log(firstImageDOMElement);
+
+// Ciclo per spostare la classe active con il right slider
+const rightArrowDOMElement = document.querySelector(".right-arrow");
+
+rightArrowDOMElement.addEventListener("click", function () {
+  const actualImageDOMElement = carouselImgDOMElements[actualImgIdx];
+  actualImageDOMElement.classList.remove("active");
+  // Se sono all'ultimo elemento
+  if (actualImgIdx === (carouselImgDOMElements.length - 1)) {
+    actualImgIdx = 0;
+  } else {
+    actualImgIdx++;
+  }
+  const nextImageDOMElement = carouselImgDOMElements[actualImgIdx];
+  nextImageDOMElement.classList.add("active");
+});
+
+// Ciclo per spostare la classe active con il left slider
+const leftArrowDOMElement = document.querySelector(".left-arrow");
+
+leftArrowDOMElement.addEventListener('click', function (){
+    const actualImageDOMElement = carouselImgDOMElements[actualImgIdx];
+    actualImageDOMElement.classList.remove('active');
+    // Se sono al primo elemento
+    if(actualImgIdx === 0){
+        actualImgIdx = (carouselImgDOMElements.length - 1);
+    } else {
+        actualImgIdx--;
+    }
+    const prevImageDOMElement = carouselImgDOMElements[actualImgIdx];
+    prevImageDOMElement.classList.add("active");
+})
